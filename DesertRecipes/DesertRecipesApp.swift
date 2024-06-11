2cc73f5a14990bc381ca10267dfa3652ffc1f2a3
SumithPevekar <sumithpevekar20@gmail.com>
2024-06-11 13:45:15 -0700

    Initial Commit

---
+++ DesertRecipes/DesertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesertRecipesApp.swift
+//  DesertRecipes
+//
+//  Created by Sumith Pevekar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DesertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
